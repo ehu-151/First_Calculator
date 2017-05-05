5113c51d4480eaef3dadb7075f115d8a76d454b6
ehu <fehuehuf@gmail.com>
2017-05-05 16:51:16 +0900

    小数点の計算機能実装

--- app/src/main/java/com/example/ehu/first_calculator/MainActivity.java
+++ app/src/main/java/com/example/ehu/first_calculator/MainActivity.java
@@ -7,13 +7,20 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     //一時保存
-    int temp = 0;
+    float temp = 0;
     //a+b, a-bなど
-    int a = 0, b = 0;
+    float a = 0, b = 0;
     //計算結果
-    int sum;
+    float sum;
     //記号を判断する
     int markCheck = 0;
+
+    //resultViewに表示する変数
+    String result = "";
+    //小数点フラグ
+    boolean isPointNum = false;
+    //小数入力時に入力値の位をずらすための
+    float pointCnt = 10;
 
     //電卓の計算結果のTextView
     TextView resultText;
@@ -27,17 +34,7 @@ public class MainActivity extends AppCompatActivity {
         resultText = (TextView) findViewById(R.id.resultText);
     }
 
-    //数字を押した時の処理
-    public void numClick(View v) {
-        if (resultText.length() < 9) {
-            //タグを文字列に変換
-            String numString = String.valueOf(v.getTag());
-            //文字列をintへ変換する
-            temp = temp * 10 + Integer.parseInt(numString);
-            //TextViewにnumの値を反映させる
-            resultText.setText(String.valueOf(temp));
-        }
-    }
+
 
     //記号を押した時の処理
     public void markClick(View v) {
@@ -50,6 +47,12 @@ public class MainActivity extends AppCompatActivity {
         a = temp;
         //tempの初期化
         temp = 0;
+        //resultの初期化
+        result = "";
+        //小数点のフラグを初期化
+        isPointNum = false;
+        //pointCntの初期化
+        pointCnt = 10;
     }
 
     //=を押した時の処理
@@ -58,6 +61,8 @@ public class MainActivity extends AppCompatActivity {
         b = temp;
         //tempの初期化
         temp = 0;
+        //小数点のフラグを初期化
+        isPointNum = false;
 
         //mark_Checkに対応した四則演算をさせる
         switch (markCheck) {
@@ -79,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         //sumの値をresultTextに反映させる
-        resultText.setText(String.valueOf(sum));
+        resultText.setText(conFormat(String.valueOf(sum)));
     }
 
     //Cを押した時の処理
@@ -90,7 +95,32 @@ public class MainActivity extends AppCompatActivity {
         temp = 0;
         sum = 0;
 
-        //sumの値をresultTextに反映させる
-        resultText.setText(String.valueOf(sum));
+        //小数点のフラグを初期化
+        isPointNum = false;
+        //pointCntの初期化
+        pointCnt = 10;
+        //resultTextに反映させる
+        result = "";
+        resultText.setText("0");
+    }
+
+    //小数点メソッド
+    public void pointNum(View v) {
+        if (isPointNum == false) {
+            //小数点をつける
+            result = result + ".";
+            //小数点をつけたフラグ
+            isPointNum = true;
+            //TextViewにnumの値を反映させる
+            resultText.setText(result);
+        }
+    }
+
+    //「.0」を削除するメソッド
+    public String conFormat(String stSum) {
+        if (stSum.endsWith(".0") == true) {
+            stSum = stSum.substring(0, stSum.length() - 2);
+        }
+        return stSum;
     }
 }
